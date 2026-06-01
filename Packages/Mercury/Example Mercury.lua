78e9193b13c1578869b621dc2e3087a740ed46a2
Paazlis <paazlismaswa@gmail.com>
2026-06-01 13:25:28 +0700

    Example Mercury.lua

---
+++ Packages/Mercury/Example Mercury.lua
@@ -0,0 +1,85 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/PaazlisMaswa/RobloxProject/Packages/Mercury/init.luaau"))()
+
+local Window = Library:Create({
+    Theme = Library.Themes.Serika
+})
+
+local Tab = Window:Tab({
+    Icon = "rbxassetid://6034996695",
+    Name = "Aimbot"
+}
+
+Tab:Button({
+    Name = "show prompt",
+    Callback = function()
+        tab:Prompt{
+            Title = "baby",
+            Text = "nice to meet you",
+            Buttons = {
+                Ok = function()
+                    tab:prompt{
+                        Followup = true,
+                        Title = "really?",
+                        Text = "you sure?=",
+                        Buttons = {
+                            Yes = function()
+                                tab:Prompt{
+                                    Followup = true,
+                                    Title = "lol",
+                                    Text = "Welcome",
+                                    Buttons = {
+                                        Learn = function()
+                                            gui:set_status("money")
+                                        end,
+                                        Lazy = function()
+                                            gui:set_Status("homeless")
+                                        end
+                                    }
+                                }
+                            end,
+                        }
+                    }
+                end,
+            }
+        }
+    end
+})
+
+Tab:Keybind({Callback = function()
+    gui:prompt()
+end})
+
+Tab:Dropdown({
+    Name = "Dropdown",
+    Description = "Dropdown Description",
+    StartingText = "Bodypart",
+    Items = {
+        "Head",
+        "Torso",
+        "Random"
+    }
+})
+
+Tab:slider({
+    Name = "Slider",
+    Description = "Slider Description",
+    StartingText = "Aimbot Speed",
+    Callback = function(v)
+       gui:set_status(v)
+    end
+})
+
+Tab:Textbox({Callback = function(v)
+    gui:Prompt{Text = v}
+end,})
+
+Tab:color_picker({
+    Name = "Color Picker",
+    Style = Library.ColorPickerStyles.Legacy,
+    Description = "Click to adjust color...",
+    Callback = function(color)
+        print(color)
+    end,
+})
+
+Tab:Toggle({})
